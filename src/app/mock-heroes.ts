0b81ce3c8301fb5f5d75ca4e3f19d23c5f6675cd
German Dominguez <gdominguezmontes@gmail.com>
2018-09-27 17:42:48 -0500

    Avances

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -2,7 +2,7 @@ import { Hero } from './hero';
 export const HEROES: Hero[] = [
   {id: 11, name: 'Mr. Nice'},
   {id: 12, name: 'Narco'},
-  {id: 13, name: 'Bormbardo'},
+  {id: 13, name: 'Bombardo'},
   {id: 14, name: 'Celeritas'},
   {id: 15, name: 'Magenta'},
   {id: 16, name: 'RubberMan'},
